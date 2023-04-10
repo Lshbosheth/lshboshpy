e03822bd3cdc59d0aeff12769a8dd50a2c009599
lshbosheth <lizezhi@jk366.net>
2023-04-10 20:34:37 +0800

    fix

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from routers import items, idCard
-# app = FastAPI(openapi_prefix="/api/")
-app = FastAPI()
+import os
+
+mode = os.environ.get('mode')
+
+if mode == 'prod':
+    app = FastAPI(openapi_prefix="/api/")
+else:
+    app = FastAPI()
 app.include_router(items.router)
 app.include_router(idCard.router)
