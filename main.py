c70f34be6ebb0e3dc43f957e23d9aaa3d0aafabb
lshbosheth <lizezhi@jk366.net>
2023-04-10 17:27:43 +0800

    fix

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ app = FastAPI()
 
 @app.get("/items/")
 async def read_items(q: Union[List[str], None] = Query(default=None)):
-    query_items = {"q": q}
+    query_items = {"q3": q}
     return query_items
