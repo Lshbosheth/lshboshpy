bae78e58b96a100458eb6ea00a458186d4bf204c
lshbosheth <lizezhi@jk366.net>
2023-04-21 20:01:47 +0800

    修改swagger静态文件地址

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi import FastAPI, applications
 from routers import items, idCard, dbtest, qrCode
 import os
 
+
+def swagger_monkey_patch(*args, **kwargs):
+    return get_swagger_ui_html(
+        *args, **kwargs,
+        swagger_js_url='https://cdn.bootcdn.net/ajax/libs/swagger-ui/4.10.3/swagger-ui-bundle.js',
+        swagger_css_url='https://cdn.bootcdn.net/ajax/libs/swagger-ui/4.10.3/swagger-ui.css'
+    )
+
+
+applications.get_swagger_ui_html = swagger_monkey_patch
 mode = os.environ.get('mode')
 
 if mode == 'prod':
